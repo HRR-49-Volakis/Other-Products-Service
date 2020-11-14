9a550bd19735226a8e3c68a3692ff9840870ce15
Taylor Anderson <tayloranderson1265@gmail.com>
2020-11-13 19:43:37 -0500

    Finished setting up react with webpack and rendered "Hello world" from the app component.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const SRC = path.join(__dirname, '/client/src/');
+const PUBLIC = path.join(__dirname, '/public/');
+
+module.exports = {
+
+  mode: 'development',
+  entry: `${SRC}/index.js`,
+  watch: true,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(`${PUBLIC}`)
+  },
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    }]
+  }
+
+
+}
