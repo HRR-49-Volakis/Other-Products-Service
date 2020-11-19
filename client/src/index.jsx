6d3fc4a7cff0c3ca33899c7573d88cba3f543696
Taylor Anderson <tayloranderson1265@gmail.com>
2020-11-19 01:28:21 -0500

    added a scroll effect

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx';
 import ProductList from './components/ProductList.jsx';
 import Product from './components/Product.jsx';
 
+App.style
+
 const mainProduct = '44';
 ReactDOM.render(
   <App
