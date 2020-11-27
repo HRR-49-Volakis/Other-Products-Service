93c014f2446e8e682ebc4a45c75534be3a92fca9
Taylor Anderson <tayloranderson1265@gmail.com>
2020-11-27 13:29:06 -0500

    changed initial item id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-const mainProduct = '33';
+const mainProduct = '89';
 ReactDOM.render(
   <App
     mainProductId={mainProduct}
