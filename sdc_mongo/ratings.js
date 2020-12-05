2f4f1a30ccb603ed99538256db7a49a956023d57
Alysa Shin <ashin.saga@gmail.com>
2020-12-05 07:06:29 -0800

    mostly comments, need to refactor later

---
+++ sdc_mongo/ratings.js
@@ -0,0 +1,16 @@
+// require the array from seed.js
+
+// mongoose insertMany
+  // require index.js OR schema.js? (want access to models)
+  // var data = [ { field: 'value', field: 'value'}];
+  // Rating.inserMany(data, function(err, result) {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+
+  //   console.log('number of documents inserted:', result.insertedCount);
+  // });
+    // insertMany returns an object with some properties
+    // ex. result.insertedCount (line 11)
+
+// time the seeding process for journal
--- sdc_mongo/schema.js
+++ sdc_mongo/schema.js
@@ -1,17 +1,40 @@
-const db = require('./index.js');
+// const mongo = require('mongodb');
+// const mongoose = require('mongoose');
+// const { db } = require('./index.js');
 
-db.once('open', function() {
-  const productSchema = new mongoose.Schema({
-    id: Number,
-    product_name: String,
-    image_one_url: String,
-    image_two_url: String,
-    page_url: String,
-    price: Number,
-    hearted: Boolean,
-    brief_description: String,
-    collection_name: String
-  });
+// const productSchema = new mongoose.Schema({
+//   id: Number,
+//   product_name: String,
+//   image_one_url: String,
+//   image_two_url: String,
+//   page_url: String,
+//   price: Number,
+//   hearted: Boolean,
+//   brief_description: String,
+//   collection_name: String
+// });
+
+// const ratingSchema = new mongoose.Schema({
+//   id: Number,
+//   rated_product: Number,
+//   stars_given: Number
+// })
+
+// const Product = mongoose.model('Product', productSchema);
+// const Rating = mongoose.model('Rating', ratingSchema);
+
+// const func = function(values) {
+//   Rating.insertMany(values, function(err, result) {
+//     if (err) {
+//       console.log('ERROR---', err);
+//     } else {
+//       console.log('Documents successfully inserted. Number of insertions: ', result.insertedCount);
+//     }
+//   })
+// }
 
 
-})
+// // need to export the models in order to acces it in other files?
+// module.exports = {
+//   func
+// }
